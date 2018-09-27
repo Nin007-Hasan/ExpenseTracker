28e362c7f626672ba60b4863cc24ceb26a25322a
hihasan <hasan@hihasan.com>
2018-09-27 19:12:19 +0600

    Kotlin project

---
+++ app/src/main/java/com/hihasan/expensetracker/NoInternet.kt
@@ -0,0 +1,12 @@
+package com.hihasan.expensetracker
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class NoInternet: AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nointernet)
+    }
+}
