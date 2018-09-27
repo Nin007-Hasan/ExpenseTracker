d8a5978967430c51ed5804dc559306b4e29fd94d
hihasan <hasan@hihasan.com>
2018-09-27 20:02:05 +0600

    Check Internet Status

--- app/src/main/java/com/hihasan/expensetracker/StartupActivity.kt
+++ app/src/main/java/com/hihasan/expensetracker/StartupActivity.kt
@@ -1,5 +1,9 @@
 package com.hihasan.expensetracker
 
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
@@ -10,6 +14,35 @@ class StartupActivity: AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        checkConnection()
+
+
+
+
+    }
+
+    private fun checkConnection() {
+        if (isOnline()==true){
+            val i=Intent(applicationContext,MainActivity::class.java)
+            startActivity(i)
+        }
+
+        else{
+            val i=Intent(applicationContext,NoInternet::class.java)
+            startActivity(i)
+
+        }
+
+    }
+
+    protected fun isOnline(): Boolean {
+        val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val netInfo = cm.activeNetworkInfo
+        return if (netInfo != null && netInfo.isConnectedOrConnecting) {
+            true
+        } else {
+            false
+        }
     }
 
 }
